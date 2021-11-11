dbaf5de5301397add985211a304a71ce855b7c30
Evgeny Deriglazov <evgenyderiglazov@gmail.com>
2021-11-11 20:45:33 +0300

    First commit

---
+++ one/etl_init.py
@@ -0,0 +1,219 @@
+#!/usr/bin/env python3
+
+"""ETL init script for one shippings."""
+"""Loads new records into one database, tracking and init collections."""
+
+import requests
+import time
+from datetime import datetime
+from pymongo import MongoClient
+from pymongo.errors import ConnectionFailure
+from bson.json_util import dumps
+
+# MongoDB connection details
+USER = "OneInit"
+PWD = "<tkfzDtcnf844"
+HOST = "194.58.102.147"
+URI = "mongodb://{}:{}@{}:27017/".format(USER,PWD,HOST)
+
+# External data resource
+URL = "https://ecomm.one-line.com/ecom/CUP_HOM_3301GS.do"
+
+def log(message):
+    """Log function to log errors."""
+    timestamp = datetime.strftime(datetime.now(), "%Y-%m-%d %H:%M:%S")
+    with open("etl.log", "a") as f:
+        f.write(timestamp + " " + message + "\n")
+
+def check_record(bill_number):
+    """Check that init and tracking database
+    does not have container record yet."""
+    conn = MongoClient(URI)
+    query = {"blNo": bill_number, "trackEnd": None}
+    try:
+        conn.admin.command("ping")
+        init = conn.one.init.count_documents(query)
+        tracking = conn.one.tracking.count_documents(query)
+        if init == 0 and tracking == 0:
+            conn.close()
+            return True
+        else:
+            conn.close()
+            log(f"[ETL Init] [Check record]"\
+                + f" [Record already exists for {bill_number}]")
+            return False
+    except ConnectionFailure:
+        log("[ETL Init] [Check record]"\
+            + f" [DB Connection failure for {bill_number}]")
+        conn.close()
+        return False
+    except BaseException as err:
+        log("[ETL Init] [Check record]"\
+            + f" [{err.details} for {bill_number}]")
+        conn.close()
+        return False
+
+def extract_container_details(bill_number):
+    """Post request to extract container details."""
+    if isinstance(bill_number, str):
+        # Create payload for get request
+        payload = {
+            '_search': 'false', 'nd': str(time.time_ns())[:-6],
+            'rows': '10000', 'page': '1', 'sidx': '',
+            'sord': 'asc', 'f_cmd': '121', 'search_type': 'A',
+            'search_name': bill_number, 'cust_cd': '',
+        }
+        # Run request and fetch json data
+        r = requests.get(URL, params=payload)
+        data = r.json()
+        # Extract container details data
+        if "list" in data:
+            container_details = data["list"][0]
+            # Remove unnecessary data
+            if "hashColumns" in container_details:
+                del container_details["hashColumns"]
+            return container_details
+        else:
+            log(f"[ETL Init] [Extract container details]"\
+                + f" [No details data for {bill_number}]")
+            return False
+    else:
+        log("[ETL Init] [Extract container details]"\
+            + f" [Wrong argument type {bill_number}]")
+        return False
+
+def extract_schedule_details(cntr_details):
+    """Extract schedule details."""
+    if cntr_details:
+        # Create payload for get request
+        payload = {
+            '_search': 'false', 'f_cmd': '125', 'cntr_no': cntr_details["cntrNo"],
+            'bkg_no': '', 'cop_no': cntr_details["copNo"]
+        }
+        # Run request and fetch json data
+        r = requests.get(URL, params=payload)
+        data = r.json()
+        # Extract container schedule data
+        if "list" in data:
+            schedule_details = data["list"]
+            if "hashColumns" in schedule_details[0]:
+                del schedule_details[0]["hashColumns"]
+            return schedule_details
+        else:
+            log("[ETL Init] [Extract schedule details]"\
+                + f" [No schedule for container {cntr_details['cntrNo']}]")
+            return False
+    else: 
+        return False
+
+def extract(bill_number):
+    """Extract container and schedule details and
+    return one document."""
+    cntr_details = extract_container_details(bill_number)
+    schedule_details = extract_schedule_details(cntr_details)
+    if cntr_details and schedule_details:
+        return {"container": cntr_details,
+                "schedule": schedule_details,
+                "number": bill_number}
+    else:
+        log("[ETL Init] [Extract phase]"\
+            + f" [No data for {bill_number}]")
+        return False
+
+def transform(data):
+    """Transforms raw data for database load."""
+    # Check data argument
+    if not data:
+        log("[ETL Init] [Transform phase]"\
+            + f" [No raw data]")
+        return False
+    
+    # Check contnainer keys and extract container info
+    cntr_keys = ["cntrNo", "cntrTpszNm", "copNo", "blNo"]
+    if set(cntr_keys).issubset(set(data["container"])):
+        result = {
+            "cntrNo": data["container"]["cntrNo"],
+            "cntrType": data["container"]["cntrTpszNm"],
+            "copNo": data["container"]["copNo"],
+            "blNo": data["container"]["blNo"],
+            "trackStart": datetime.now().replace(microsecond=0),
+            "trackEnd": None,
+            "outboundTerminal": "",
+            "inboundTerminal": "",
+            "schedule": None,
+        }
+    else:
+        log("[ETL Init] [Transform]"\
+            + f" [Keys do not match in container data {data['number']}]")
+        return False
+    
+    # Check schedule keys and extract schedule data
+    schedule_keys = ["no", "statusNm", "placeNm", "yardNm",
+                     "eventDt", "actTpCd", "actTpCd", "vslEngNm",
+                     "lloydNo"]
+    if set(schedule_keys).issubset(set(data["schedule"][0])):
+        schedule = [{
+            "no": i["no"],
+            "event": i["statusNm"],
+            "placeName": i["placeNm"],
+            "yardName": i["yardNm"],
+            "eventDate": datetime.strptime(i["eventDt"], "%Y-%m-%d %H:%M"),
+            "status": i["actTpCd"],
+            "vesselName": i["vslEngNm"],
+            "imo": i["lloydNo"],
+        } for i in data["schedule"]]
+        result["schedule"] = schedule
+        # Find and save outbound and inbound terminals
+        for i in data["schedule"]:
+            if i["statusNm"].find("Outbound Terminal") > -1:
+                result["outboundTerminal"] = i["placeNm"]\
+                + "|" + i["yardNm"]
+            if i["statusNm"].find("Inbound Terminal") > -1:
+                result["inboundTerminal"] = i["placeNm"]\
+                + "|" + i["yardNm"]
+    else:
+        log("[ETL Init] [Transform]"\
+            + f" [Keys do not match in schedule data {data['number']}]")
+        return False
+    return result
+
+def load(data):
+    """Loads data into init and tracking collections."""
+    # Check data argument
+    if not data:
+        log("[ETL Init] [Load] [No data to load]")
+        return None
+    
+    # Connect to database and load data
+    conn = MongoClient(URI)
+    try:
+        conn.admin.command("ping")
+        cur_init = conn.one.init.insert_one(data)
+        if cur_init.acknowledged == False:
+            log("[ETL Init] [Load] "\
+                + f"[{data['blNo']} not loaded to init]")
+        cur_tracking = conn.one.tracking.insert_one(data)
+        if cur_tracking.acknowledged == False:
+            log("[ETL Init] [Load] "\
+                + f"[{data['blNo']} not loaded to tracking]")
+        conn.close()
+    except ConnectionFailure:
+        log("[ETL Init] [Load] "\
+            + f"[Connection failure for {data['blNo']}]")
+        conn.close()
+    except BaseException as err:
+        log("[ETL Init] [Load] "\
+            + f"[{err.details} for {data['blNo']}]")
+        conn.close()
+
+def main(args):
+    """ETL init pipeline."""
+    if len(args) > 0:
+        for arg in args:
+            if check_record(arg):
+                raw_data = extract(arg)
+                transformed_data = transform(raw_data)
+                load(transformed_data)
+
+if __name__ == '__main__':
+    sys.exit(main(sys.argv[1:]))
---
+++ one/etl_update.py
@@ -0,0 +1,153 @@
+#!/usr/bin/env python3
+
+"""ETL update script for one shippings."""
+"""Updates records in one database, tracking collection."""
+
+import requests
+from datetime import datetime
+from pymongo import MongoClient
+from pymongo.errors import ConnectionFailure
+from bson.json_util import dumps
+import sys
+import json
+
+# MongoDB connection details
+USER = "OneUpdate"
+PWD = "<tkfzDtcnf844"
+HOST = "194.58.102.147"
+URI = "mongodb://{}:{}@{}:27017/".format(USER,PWD,HOST)
+
+# External data resource
+URL = "https://ecomm.one-line.com/ecom/CUP_HOM_3301GS.do"
+
+def log(message):
+    """Log function to log errors."""
+    timestamp = datetime.strftime(datetime.now(), "%Y-%m-%d %H:%M:%S")
+    with open("etl.log", "a") as f:
+        f.write(timestamp + " " + message + "\n")
+
+def records_to_update():
+    """Prepare records which require update."""
+    # Prepare connection, query and project fields
+    conn = MongoClient(URI)
+    now = datetime.now().replace(microsecond=0)
+    query = {
+        "trackEnd": None,
+        "schedule": {"$elemMatch": {"status": "E", "eventDate": {"$lte": now}}}
+    }
+    project = {"cntrNo": 1, "copNo": 1, "_id": 0}
+    # Query database
+    try:
+        conn.admin.command("ping")
+        cur = conn.one.tracking.find(query, project)
+        records = json.loads(dumps(cur))
+        conn.close()
+        if len(records) > 0:
+            return records
+        else:
+            return False
+    except ConnectionFailure:
+        log("[ETL Update] [Records to update] "\
+            + f"[DB Connection failure]")
+        conn.close()
+        return False
+    except BaseException as err:
+        log("[ETL Update] [Records to update] "\
+            + f"[{err.details}]")
+        conn.close()
+        return False
+
+def extract_schedule_details(records):
+    """Extract schedule details for update."""
+    # Check input
+    if not records:
+        return False
+    # Extract data
+    for rec in records:
+        # Create payload for get request
+        payload = {
+            '_search': 'false', 'f_cmd': '125', 'cntr_no': rec["cntrNo"],
+            'bkg_no': '', 'cop_no': rec["copNo"]
+        }
+        # Run request and fetch json data
+        r = requests.get(URL, params=payload)
+        data = r.json()
+        # Extract container schedule data and clean
+        if "list" in data:
+            schedule_details = data["list"]
+            if "hashColumns" in schedule_details[0]:
+                del schedule_details[0]["hashColumns"]
+            rec["schedule"] = schedule_details
+        else:
+            log("[ETL Update] [Extract schedule details]"\
+                + f" [No schedule for container {rec['cntrNo']}]")
+            rec["schedule"] = None
+    return records
+
+def transform(records):
+    """Transforms raw data for database load."""
+    # Check input
+    if not records:
+        return False
+    # Check schedule keys and extract schedule data
+    schedule_keys = ["no", "statusNm", "placeNm", "yardNm",
+                     "eventDt", "actTpCd", "actTpCd", "vslEngNm",
+                     "lloydNo"]
+    for rec in records:
+        if set(schedule_keys).issubset(set(rec["schedule"][0])):
+            schedule = [{
+                "no": i["no"],
+                "event": i["statusNm"],
+                "placeName": i["placeNm"],
+                "yardName": i["yardNm"],
+                "eventDate": datetime.strptime(i["eventDt"], "%Y-%m-%d %H:%M"),
+                "status": i["actTpCd"],
+                "vesselName": i["vslEngNm"],
+                "imo": i["lloydNo"],
+            } for i in rec["schedule"]]
+            rec["schedule"] = schedule
+        else:
+            log("[ETL Update] [Transform] "\
+                + f"[Keys do not match in schedule data {rec['cntrNo']}]")
+            rec["schedule"] = None
+    return records
+
+def update(records):
+    """Update records in database."""
+    # Check input
+    if not records:
+        return False
+    # Connect to database and update
+    conn = MongoClient(URI)
+    try:
+        conn.admin.command("ping")
+        for rec in records:
+            if rec["schedule"]:
+                query = {"cntrNo": rec["cntrNo"]}
+                change = {"$set": {"schedule": rec["schedule"]}}
+                cur_tracking = conn.one.tracking.update_one(query, change)
+                if cur_tracking.acknowledged == False:
+                    log("[ETL Update] [Update] "\
+                    + f"[{rec['cntrNo']} not updated in tracking]")
+            else:
+                log("[ETL Update] [Update] "\
+                + f"[Not updated {rec['cntrNo']}]")
+    except ConnectionFailure:
+        for rec in records:
+            log("[ETL Update] [Update] "\
+                + f"[Connection failure for {rec['cntrNo']}]")
+    except BaseException as err:
+        for rec in records:
+            log("[ETL Update] [Update] "\
+                + f"[{err.details} for {rec['cntrNo']}]")
+    conn.close()
+
+def main():
+	"""ETL update pipeline"""
+	records = records_to_update()
+	raw_records = extract_schedule_details(records)
+	transformed_records = transform(raw_records)
+	update(transformed_records)
+
+if __name__ == '__main__':
+	sys.exit(main())
