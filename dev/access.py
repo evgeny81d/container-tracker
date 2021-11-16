a5e72f1572f7bc875a40a5e67456e8a1e0280224
Evgeny Deriglazov <evgenyderiglazov@gmail.com>
2021-11-16 18:54:14 +0300

    New access.py file

--- dev/access.py
+++ dev/access.py
@@ -3,13 +3,13 @@
 
 # Init operations
 init = "mongodb://{}:{}@{}:27017/"\
-	.format("OneInit", "<tkfzDtcnf844", "194.58.102.147")
+	.format("username", "password", "host")
 
 # Update operations
 update = "mongodb://{}:{}@{}:27017/"\
-.format("OneUpdate", "<tkfzDtcnf844", "194.58.102.147")
+	.format("username", "password", "host")
 
 # Track end operations
 track_end = "mongodb://{}:{}@{}:27017/"\
-	.format("TrackEnd", "<tkfzDtcnf844", "194.58.102.147")
+	.format("username", "password", "host")
 	
