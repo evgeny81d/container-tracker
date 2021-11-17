0c2b5678958cc8711cb85eedf345ccc76c99895b
Evgeny Deriglazov <evgenyderiglazov@yandex.ru>
2021-11-17 09:15:49 +0300

    .ginignore

---
+++ dev/access.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+# File with access credentials for MongoDB
+
+# Init operations
+init = "mongodb://{}:{}@{}:27017/"\
+	.format("user", "pwd", "host")
+
+# Update operations
+update = "mongodb://{}:{}@{}:27017/"\
+	.format("user", "pwd", "host")
+
+# Track end operations
+track_end = "mongodb://{}:{}@{}:27017/"\
+	.format("user", "pwd", "host")
+	
