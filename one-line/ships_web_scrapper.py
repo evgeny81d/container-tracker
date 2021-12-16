07cd65a7ac6643888a66fbac44f9faf76d12adac
Evgeny Deriglazov <evgenyderiglazov@gmail.com>
2021-12-16 22:26:37 +0300

    Update ships_web_scrapper.py to working version.

--- one-line/ships_web_scrapper.py
+++ one-line/ships_web_scrapper.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python3
+
 import requests
 from datetime import datetime
 from pymongo import MongoClient
@@ -17,76 +19,83 @@ def log(message):
 
 def request_web_page(ship_id):
     """Request web page for ship_id."""
-    return requests.get(URL + ship_id, headers={"User-Agent": "Mozilla/5.0"})
+    response = requests.get(
+        URL + str(ship_id),
+        headers={"User-Agent": "Mozilla/5.0"}
+    )
+    return response
 
-def parse_title(response):
-    """Parse title string from html response."""
-    response = BeautifulSoup(response.text, "html.parser")
-    return response.title.text
+def get_page_title(response, ship_id):
+    """Get page title string from html response."""
+    if response.status_code == 200:
+        soup = BeautifulSoup(response.text, "html.parser")
+        return soup.title.text
+    else:
+        log("[ships_web_scrapper.py] [get_page_title()] " \
+                + f"[{response.status_code} for ship id {ship_id}]")
+        return False
 
-def parse_ship_details(title):
-    """Parse ship details from web page title."""
+def scrap_ship_details(title, ship_id):
+    """Scrap ship details from web page title."""
+    if not title:
+        return False
     name_str = title[title.find("Ship") + 5:title.find("Registered in")].strip()
     name = name_str.split("(")[0].strip()
     ship_type = name_str.split("(")[1].replace(")", "")
     flag = title[title.find("Registered in") + 13:title.find("-")].strip()
-    imo = title[title.find("IMO") + 3: title.find("MMSI") - 2].strip()
-    mmsi = title[title.find("MMSI") + 4: title.find("MMSI") + 14].strip()
+    imo = title[title.find("IMO ") + 3: title.find("MMSI ") - 2].strip()
+    if len(imo) == 7 and imo.isdigit():
+        imo = int(imo)
+    else:
+        log("[ships_web_scrapper.py] [scrap_ship_details()] " \
+                + f"[Incorrect imo {imo} for ship id {ship_id}]")
+        imo = 0
+    mmsi = title[title.find("MMSI ") + 4: title.find("MMSI ") + 14].strip()
+    if len(mmsi) == 9 and mmsi.isdigit():
+        mmsi = int(mmsi)
+    else:
+        log("[ships_web_scrapper.py] [scrap_ship_details()] " \
+                + f"[Incorrect mmsi {mmsi} for ship id {ship_id}]")
+        mmsi = 0
     call_sign = title[title.find("Call Sign") + 9:].strip()
     result = {
-        "ship_id": "", "name": name, "type": ship_type, "flag": flag,
+        "ship_id": ship_id, "name": name, "type": ship_type, "flag": flag,
         "imo": imo, "mmsi": mmsi, "callSign": call_sign
     }
     return result
 
-def web_scrapper(ship_ids):
-    """Scrap data from marinetraffic.com by ship id number."""
-    result = []
-    for ship_id in ship_ids:
-        response = request_web_page(ship_id)
-        if response.status_code == 200:
-            #print(ship_id)
-            #print(response.text)
-            title = parse_title(response)
-            ship_details = parse_ship_details(title)
-            ship_details["ship_id"] = ship_id
-            result.append(ship_details)
-        else:
-            log("[Ships_web_scrapper.py] [web_scrapper()] " \
-                + f"[No ship for ship id {ship_id}]")
-    return result
-
-def insert_ship_to_db(ships):
+def insert_ship_to_db(ship):
     """Insert ship record to db."""
+    if not ship:
+        return
     # Connect to database and update data
     conn = MongoClient(access.update)
     try:
         conn.admin.command("ping")
         now = datetime.now().replace(microsecond=0)
-        for ship in ships:
-            ship_copy = ship.copy()
-            #del ship_copy["cntrNo"]
-            ship_copy["lastUpdate"] = now
-            cur = conn.one.ships.insert_one(ship_copy)
-            if cur.acknowledged == False:
-                log("[Update ship location] [Insert ship to db] "\
-                    + f"[Imo {ship['imo']} not inserted]")
+        ship["update"] = now
+        cur = conn.one.ships.insert_one(ship)
+        if cur.acknowledged == False:
+            log("[ships_web_scrapper.py] [insert_ship_to_db()] "\
+                + f"[Ship id {ship['ship_id']} imo {ship['imo']} not inserted]")
         conn.close()
     except ConnectionFailure:
-        log("[Update ship location] [Insert ship to db] "\
-            + "[DB Connection failure for imo "\
-            + f"{ship['imo']}, mmsi {ship['mmsi']}]")
+        log("[ships_web_scrapper.py] [insert_ship_to_db()] "\
+            + f"[DB Connection failure for ship id {ship['ship_id']} imo {ship['imo']}]")
         conn.close()
     except BaseException as err:
-        log("[Update ship location] [Insert ship to db] "\
-            + f"[{err} for imo {ship['imo']}, mmsi {ship['mmsi']}]")
+        log("[ships_web_scrapper.py] [insert_ship_to_db()] "\
+            + f"[{err} for ship id {ship['ship_id']} imo {ship['imo']}]")
         conn.close()
 
+
 def main():
-    for idx, i in enumerate(range(10,9999999,10)):
-        ship_ids = [str(ii) for ii in range(idx * 10,i)]
-        ships = web_scrapper(ship_ids)
-        insert_ship_to_db(ships)
+    """ETL data pipeline."""
+    for ship_id in range(999999):
+        response = request_web_page(ship_id)
+        title = get_page_title(response, ship_id)
+        ship = scrap_ship_details(title, ship_id)
+        insert_ship_to_db(ship)
 
 if __name__ == '__main__':
     sys.exit(main())
